828810581f035db903a9b97824614ff0074456b5
Varun Sreedhar <62417466+VarunS9000@users.noreply.github.com>
2020-11-22 23:33:13 +0530

    Add files via upload

--- Binary Search Tree.py
+++ Binary Search Tree.py
@@ -36,12 +36,26 @@ def postorder(root):
         postorder(root.right)
         print(root.value)
 
+def search(root,x):
+    if root:
+        if root.value==x:
+            print("found")
+
+        elif x<root.value:
+            search(root.left,x)
+
+        elif x>root.value:
+            search(root.right,x)
+
+    else:
+        print("element does not exist")
+
 y=int(input("enter tree root value"))
 
 Root=Node(y)
               
 
-print("1.Insert Node\n2.Inorder\n3.Preorder\n4.Postorder")
+print("1.Insert Node\n2.Inorder\n3.Preorder\n4.Postorder\n5.Search")
 while True:
     x=int(input("Enter choice"))
 
@@ -57,6 +71,10 @@ while True:
 
     elif(x==4):
         postorder(Root)
+
+    elif(x==5):
+        m=int(input("enter element to be searched"))
+        search(Root,m)
 
     elif(x==0):
         break
