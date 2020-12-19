baabfea4f5e29ee56eb285e7dd2f87526f498e24
Varun Sreedhar <62417466+VarunS9000@users.noreply.github.com>
2020-12-19 11:28:33 +0530

    Add files via upload

--- Dijkstra's Algo.py
+++ Dijkstra's Algo.py
@@ -1,5 +1,6 @@
 #Dijkstra's Algorithm
 
+
 class Graph:
     def __init__(self,vertices):
         self.V=vertices
@@ -15,6 +16,20 @@ class Graph:
 
         return min_index
 
+    def showPath(self,node,src,parent):
+
+       
+
+        if(parent[node]==None):
+            print(src)
+            return
+
+        else:
+            print(node)
+            self.showPath(parent[node],src,parent)
+
+        
+
     
 
 
@@ -24,7 +39,8 @@ class Graph:
         d=[10000]*self.V
         l=[False]*self.V
         d[src]=0
-        
+        parent=[-1]*self.V
+        parent[src]=None
 
     
        
@@ -36,12 +52,32 @@ class Graph:
              for v in range(self.V):
                  if(self.graph[u][v]>0 and l[v]==False and d[v]>d[u]+self.graph[u][v]):
                      d[v]=d[u]+self.graph[u][v]
-                     
+                     parent[v]=u
                      
 
 
         for node in range(self.V):
             print(node,"\t",d[node])
+
+        print(parent)
+
+
+        while(True):
+            print("Enter -1 to leave")
+            x=int(input("Enter node to view it's path"))
+            if(x==-1):
+                break
+
+            else:
+                self.showPath(x,src,parent)
+            
+            
+
+
+        
+
+
+        
 
         
 
@@ -57,10 +93,12 @@ g.graph=[[0, 4, 0, 0, 0, 0, 0, 8, 0],
         [0, 0, 4, 14, 10, 0, 2, 0, 0], 
         [0, 0, 0, 0, 0, 2, 0, 1, 6], 
         [8, 11, 0, 0, 0, 0, 1, 0, 7], 
-        [0, 0, 2, 0, 0, 0, 6, 7, 0] 
-        ]
+        [0, 0, 2, 0, 0, 0, 6, 7, 0]]
 
-g.dijkstara(7)
+n=int(input("Enter source"))
+        
+
+g.dijkstara(n)
 
              
             
